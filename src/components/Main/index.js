2f08a7b7b23383f644380f81fd41d2df6b9b55a3
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2017-08-07 19:41:26 +0200

    Routing poc

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+} from "react-router-dom";
 
 import FrontPage from "../FrontPage";
 
 import "./styles.css";
 
-class Main extends Component {
-    render() {
-        return (
-            <FrontPage />
-        )
-    }
+function PageVielsen() {
+    return (
+        <div>
+            Fordi vi skal gifte oss borgerlig på tinghuset, så er det dessverre ikke plass til at alle vi ønsker kan være med på selve vielsen. Vielsen blir derfor en liten affære, med aller nærmeste familie.
+        </div>
+    )
+}
+
+function Main() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <Link to="/">HOME</Link>
+                    <Link to="/vielsen">VIELSEN</Link>
+                </nav>
+
+                <Route exact path="/" component={FrontPage}/>
+                <Route exact path="/vielse" component={PageVielsen}/>
+            </div>
+        </Router>
+    )
 }
 
 export default Main;
