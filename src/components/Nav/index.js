15a151f0d74bde1a4fe38f69134aabd1ddda1818
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2017-08-07 20:42:51 +0200

    navbar adjustments

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,8 +5,8 @@ import "./styles.css";
 
 function Nav() {
     return (
-        <nav>
-            <Link to="/bryllupet">Bryllupet</Link>
+        <nav className="Nav">
+            <Link to="/bryllupet">Program</Link>
             <Link to="/overnatting">Overnatting</Link>
             <Link to="/nøkkelpersoner">Nøkkelpersoner</Link>
             <Link to="/ønskeliste">Ønskeliste</Link>
