ad2652484b805d5880ef02f5c02911c11934ee76
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2017-08-28 19:50:13 +0200

    LA til info om bruk av rabatt

--- src/components/PageOvernatting/index.js
+++ src/components/PageOvernatting/index.js
@@ -11,15 +11,24 @@ function PageOvernatting() {
 
             <p>Vi har tatt kontakt med Quality Hotel Klubben i Tønsberg og laget en avtale med dem. De har reservert rom til våre gjester, som kan bestilles innen <strong>24. januar 2018</strong>.</p>
 
-            <p>Prisen på disse rommene er:</p>
-            <p>Dobbeltrom: 1095,- inkl. frokost</p>
-            <p>Enkeltrom: 895,- inkl. frokost</p>
+            <section className="Collected">
+                <p>Prisen på disse rommene er:</p>
+                <p>Dobbeltrom: 1095,- inkl. frokost</p>
+                <p>Enkeltrom: 895,- inkl. frokost</p>
+            </section>
 
             <p>Hvis dere ønsker å benytte dere av tilbudet, så oppgi denne referansekoden når dere tar kontakt med Hotel Klubben: <strong>1112r079559/60/61/62</strong></p>
 
             <p>Hvis dere ønsker å bo et annet sted, er dette selvfølgelig opp til dere!</p>
 
-            <p><a href="https://www.nordicchoicehotels.no/hotell/norge/tonsberg/quality-hotel-klubben/">https://www.nordicchoicehotels.no/hotell/norge/tonsberg/quality-hotel-klubben/</a></p>
+            <p>For å benytte dere av tilbudet må dere ta kontakt med de per telefon eller epost.</p>
+
+            <section className="Collected">
+                <p>Telefon: <a href="tel:+47 33 35 97 00">+47 33 35 97 00</a></p>
+                <p>E-post: <a href="mailto:q.klubben@choice.no">q.klubben@choice.no</a></p>
+            </section>
+
+            <p>For mer informasjon <a href="https://www.nordicchoicehotels.no/hotell/norge/tonsberg/quality-hotel-klubben/">se nettsiden deres.</a></p>
         </Page>
     )
 }
